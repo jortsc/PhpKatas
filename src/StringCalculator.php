5df7e526304318dff0f4dd2d25e9ecea959a403a
Jose Manuel Orts <jortsc@gmail.com>
2018-09-15 18:09:09 +0200

    Reached by TDD all required functionality point by point. Next time I'll do some baby steps in each reached requirement.

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,11 +4,76 @@ namespace PhpKatas;
 /**
  * @author: Jose Manuel Orts
  * @date: 09/09/2018
+ *
+ * About the Kata:
+ *
+ * String Calculator Kata by Roy Osherove.
+ *
+ * Create a simple String calculator with a method int Add(string numbers). The method can take 0, 1 or 2 numbers,
+ * and will return their sum (for an empty string, it will return 0). For example "" or "1" or "1,2".
+ * Start with the simplest test case of an empty string and move to 1 and two numbers.
+ * Remember to solve things as simply as possible so that you force yourself to write tests you did not think about.
+ * Remember to refactor after each passing test.
+ * Allow the Add method to handle an unknown amount of numbers.
+ * Allow the Add method to handle new lines between numbers (instead of commas).
+ * The following input is ok: "1\n2,3" (will equal 6)
+ * The following input is NOT ok: "1,\n" (not need to prove it - just clarifying)
+ * Support different delimiters. To change a delimiter, the beginning of the string will contain a separate line
+ * that looks like this: [delimiter]\n[numbers...], for example ;\n1;2 should return three where the default
+ * delimiter is ; .
+ * The first line is optional. All existing scenarios should still be supported.
+ * Calling Add with a negative number will throw an exception "negatives not allowed"
+ * and the negative that was passed.
+ * If there are multiple negatives, show all of them in the exception message
  */
 class StringCalculator
 {
+    const COMMA_DELIMITER = ',';
+    const SEMICOLON_DELIMITER = ';';
+    const BLANK_SPACE_DELIMITER = ' ';
+    const NEW_LINE_DELIMITER = "\n";
+
+    const SUPPORTED_DELIMITERS = [',', '+', ';', ':'];
+
+
+    /**
+     * @throws \Exception
+    */
     public function add($numbers)
     {
-        return $numbers;
+        $sum = 0;
+        if ($numbers === '') {
+            return $sum;
+        }
+
+        if (preg_match_all('/(-[0-9]+)/', $numbers, $matches)) {
+            $negativeOnes = implode(', ', $matches[0]);
+            throw new \Exception("Negatives not allowed: {$negativeOnes}");
+        }
+
+        $delimiter = self::SEMICOLON_DELIMITER;
+        $changeDelimiterPattern = '/^['. implode('|', self::SUPPORTED_DELIMITERS) . ']+\\n[0-9]+/';
+        if (preg_match($changeDelimiterPattern, $numbers) === 1) {
+            $delimiter = substr($numbers, 0, 1);
+        }
+
+        if (preg_match("/[0-9]+\s*{$delimiter}\s*\\n/", $numbers) === 1) {
+            return $sum;
+        }
+
+        $replaced = str_replace(
+            self::NEW_LINE_DELIMITER,
+            $delimiter,
+            $numbers
+        );
+
+        $numbers = explode($delimiter, $replaced);
+
+        foreach ($numbers as $number) {
+            $sum += (int) $number;
+        }
+
+
+        return $sum;
     }
 }
--- test/StringCalculatorTest.php
+++ test/StringCalculatorTest.php
@@ -27,8 +27,102 @@ class StringCalculatorTest extends TestCase
         self::assertInstanceOf(StringCalculator::class, $this->sut);
     }
 
-    public function testShouldReturnEmptyString()
+    public function testShouldReturnZero()
     {
-        static::assertEmpty($this->sut->add(""));
+        $expectedSum = 0;
+
+        static::assertEquals($expectedSum, $this->sut->add(''));
+    }
+
+    public function testShouldReturnInvocationParameter()
+    {
+        $number = '18';
+        $expectedSum = $number;
+
+        static::assertEquals($expectedSum, $this->sut->add($number));
+    }
+
+    public function testShouldReturnSumOfTwoInvocationParameters()
+    {
+        $numberA = '8';
+        $numberB = '10';
+        $expectedSum = (int) $numberA + (int) $numberB;
+
+        static::assertEquals($expectedSum, $this->sut->add("{$numberA};{$numberB}"));
+    }
+
+    /**
+     * @dataProvider differentAmountOfNumbersAndDelimitersProvider
+    */
+    public function testShouldReturnSumOfAllNumbersInString($numbersString, $expectedSum)
+    {
+
+        static::assertEquals($expectedSum, $this->sut->add($numbersString));
+    }
+
+    public function testShouldReturnSumOfAllNumberInAStringUnderstandingNewLines()
+    {
+        $numberA = '2000';
+        $numberB = '10';
+        $numberC = '8';
+        $expectedSum = (int) $numberA + (int) $numberB + (int) $numberC;
+
+        static::assertEquals($expectedSum, $this->sut->add("{$numberA}\n {$numberB}; {$numberC}"));
+    }
+
+    /**
+     * @dataProvider  nonValidInputsProvider
+    */
+    public function testShouldReturnZeroBecauseNewLineAndNoNumber($expectedSum, $nonValidInput)
+    {
+        static::assertEquals($expectedSum, $this->sut->add($nonValidInput));
+    }
+
+    public function testShouldChangeDelimiter()
+    {
+        $numberA = '2000';
+        $numberB = '10';
+        $numberC = '8';
+        $expectedSum = (int) $numberA + (int) $numberB + (int) $numberC;
+
+        static::assertEquals($expectedSum, $this->sut->add(",\n{$numberA}\n {$numberB}, {$numberC}"));
+    }
+
+    public function testShouldThrowAnExceptionNegativeNumberInString()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Negatives not allowed: -2');
+
+        $this->sut->add(",\n1\n 2, -2");
+    }
+
+    public function testShouldThrowAnExceptionNegativeNumbersInString()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Negatives not allowed: -4, -2, -5');
+
+        $this->sut->add(",\n1\n -4, -2\n5,-5");
+    }
+
+
+    public function differentAmountOfNumbersAndDelimitersProvider()
+    {
+        return [
+            ['10; 10; 10', 30],
+            [",\n2,2,2,2,2", 10],
+            [",\n8,8,8,8,8,8,8,8,8", 72],
+            [",\n8 , 8,8\n 8,8,8,8,8,8,2\n3 ,4,5,6", 92],
+            [":\n8 : 8:8\n 8:5:6", 43],
+            ["+\n8 + 8+8\n 8+5+6", 43]
+        ];
+    }
+
+    public function nonValidInputsProvider()
+    {
+        return [
+            [0, "2 ;\n"],
+            [0, "2;\n4"],
+            [0, "2 ;\n;9"],
+        ];
     }
 }
